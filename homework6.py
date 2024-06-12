836f0c4b6d325cc52b3d1163b06655ab54ddf7dc
marya2505 <masha-st1@mail.ru>
2024-06-13 00:31:11 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Sveta': 2015, 'Anna':2010, 'Gosha': 2023}
+print(my_dict)
+print(my_dict['Sveta'])
+print(my_dict.get('Marya'))
+my_dict.update({'Alyi':1995,
+                'Bot':2000})
+del my_dict['Sveta']
+print(my_dict)
+my_set = {1,2,3,4,3,2,1,2,3,1,'one','two','one',}
+print(my_set)
+my_set.add(6)
+my_set.add(8)
+my_set.remove('one')
+print(my_set)
